f97912255e4abfa14dbc227fd55338cf319fb83a
Carl-Johan Blomqvist <carl@cjblomqvist.com>
2012-08-05 20:33:27 +0200

    Add option to specify if straight streaming or streaming through the file system to knox is desired
    - Also add x-file-size header as requirement for straight streaming

--- derby-upload-middleware/index.js
+++ derby-upload-middleware/index.js
@@ -25,10 +25,18 @@ exports = module.exports = function(app, options){
       // If path doesn't match - use regular multipart
       multipart(options)(req, res, next); 
     } else {
-      // If knox is sent as parameters, then stream file straight to knox
+      // If knox is sent as parameters, then stream file to knox
       if(options.derbyUpload.knox) {
-        // By modifying onPart using fkstream
-        options.onPart = fkstream((options.derbyUpload.knox.auth || {}), (options.derbyUpload.directory || ''), (options.derbyUpload.knox.headers || {}), (options.derbyUpload.knox.callbacks || {}));
+        // Either by streaming straight to S3 without touching disk. Be aware of possible memory clogging using this method
+        // This requires x-file-size to be set as header on the request (and be valid).
+        // This also requires only one file to be uploaded per request
+        if(options.derbyUpload.knox.stream && req.headers['x-file-size']) {
+          // Streaming is done by modifying onPart using fkstream
+          options.onPart = fkstream((options.derbyUpload.knox.auth || {}), (options.derbyUpload.directory || ''), (options.derbyUpload.knox.headers || {}), (options.derbyUpload.knox.callbacks || {}));
+        } else {
+          // Or after file has been successfully saved to disk
+          // TODO: Implement
+        }
       }
 
       // At last, after modifying options, create multipart middleware with new options and call it
